c0b5f27dd2a1103d036a5f43fae6eb72d0037637
Mateus Cesca <165062319+Mcesca@users.noreply.github.com>
2025-10-29 09:22:32 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "🚀 Iniciando build do ROJO Music Player..."
 pip install -r requirements.txt
 mkdir -p data/audio data/covers static/images static/scripts templates
-echo "✅ Build concluído com sucesso!"
+echo "Build completed successfully"
+
