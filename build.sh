cf422a444960d9b70b6c87d2106878b3f46545de
Mateus Cesca <165062319+Mcesca@users.noreply.github.com>
2025-10-31 11:30:30 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-services:
-  - type: web
-    name: rojo-music
-    env: python
-    # Atualiza pip/setuptools, instala Pillow separadamente (forçando binários) e depois instala o restante do requirements.txt
-    buildCommand: pip install --upgrade pip setuptools wheel && pip install Pillow==10.3.0 --only-binary :all: && pip install -r requirements.txt
-    startCommand: gunicorn app:app
